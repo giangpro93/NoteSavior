be9cecf9f8d4a33fc914a61e03e915977957cb44
John Quitno <john.quitno@ku.edu>
2018-10-20 04:03:08 -0500

    Added create account and check for bad input

--- app/src/main/java/com/example/git/notesavior/Login.java
+++ app/src/main/java/com/example/git/notesavior/Login.java
@@ -1,11 +1,13 @@
 package com.example.git.notesavior;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -29,6 +31,28 @@ public class Login extends AppCompatActivity {
                     //CurrentActivity.this.startActivity(myIntent);
                 }
                 else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Invalid Username or Password";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
+        final Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if ((username.getText().toString().length()>0) && (password.getText().toString().length()>0)) {
+                    //put this stuff in database
+                }
+                else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Invalid Username or Password";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
             }
         });
